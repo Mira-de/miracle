ce4d79a0adb78008b418a60eef49c85b79cfe794
miracle <miraclesotade@gmail.com>
2024-12-03 17:38:19 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      // screens: {
+      //   sm: "576px",
+      //   md: "756px",
+      //   lg: "960px",
+      //   xl: "1020px",
+      //   // "2xl": "1200px",
+      // },
+    },
+  },
+  plugins: [],
+};
